2d3cd9e47597ca6f73619a04d793e3382cf06772
Matias Canepa <macanepa@miuandes.cl>
2019-04-11 22:21:20 -0400

    Wiwi

--- AngryLusho/app/models/user.rb
+++ AngryLusho/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
+
   belongs_to :profile
   has_many :validations
 end
--- AngryLusho/db/schema.rb
+++ AngryLusho/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_12_014949) do
+ActiveRecord::Schema.define(version: 2019_04_12_021155) do
 
   create_table "admins", force: :cascade do |t|
     t.string "username"
@@ -110,6 +110,8 @@ ActiveRecord::Schema.define(version: 2019_04_12_014949) do
     t.integer "profile_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "last_name"
     t.index ["profile_id"], name: "index_users_on_profile_id"
   end
 
