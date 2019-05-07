45418319b6a63833c6b3400e167a70587bc4638b
Vicentecorrea <vicorrea@miuandes.cl>
2019-05-07 01:35:00 -0400

    com

--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -1,5 +1,8 @@
 class UserProfileController < ApplicationController
   def view
+    @user = User.all[0]
+    @user_profile = Profile.where(user_id: @user.id)[0]
+    @user_posts = Post.where(user_id: @user.id)[0]
   end
 
   def modify
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,12 @@
 
 # # Users
 user01 = User.create(name:"Matias",last_name:"Canepa",email:"macanepa@miandes.cl",password:"123456789",last_access:Time.now)
+user02 = User.create(name:"Vicente",last_name:"Correa",email:"vicorrea@miuandes.cl",password:"123",last_access:Time.now)
 
 
 #Profiles
 profile01 = Profile.create(user_id:user01.id,image:"pic Mati",biography:"This is my Bio!",country:"Chile",city:"Santiago")
-
+profile02 = Profile.create(user_id:user02.id,image:"La foto del Vin",biography:"Removes the default list-style and left margin on list items (works on both <ul> and <ol>). This class only applies to immediate children list items (to remove the default list-style from any nested lists, apply this class to any nested lists as well)",country:"Italia",city:"Venecia")
 
 #Admins
 admin01 = Admin.create(user_id:user01.id,super_admin:true,geofence:nil)
