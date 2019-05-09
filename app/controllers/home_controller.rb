6241ead9c56697f8c1593b54362112afc35b87eb
Matias Canepa <macanepa@miuandes.cl>
2019-05-09 00:04:32 -0400

    HotFix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,20 +1,15 @@
 class HomeController < ApplicationController
   def home
-    @user = User.first
+
+
     @posts = Post.all
-
-    # @post_dump = Dumpster.all
-
     @posts_no_dumpster = Post.where("posts.id NOT IN (?)", Dumpster.pluck(:post_id))
     @posts = @posts_no_dumpster
-    @following_posts = @posts.where("posts.id IN (?)", Follow.where(user_id:@user.id).pluck(:post_id))
 
-
-    puts("WIWIWIWI\n\n\n\n\n")
-    puts(@posts_no_dumpster)
-    puts("Following")
-    puts(@following_posts)
-    puts("WIWIWIWI\n\n\n\n\n")
+    if(!params[:user_id].nil?)
+      @user = User.find(params[:user_id])
+      @following_posts = @posts.where("posts.id IN (?)", Follow.where(user_id:@user.id).pluck(:post_id))
+    end
 
     @comments = Comment.where(comment_id: nil)
     @replies = Comment.where.not(comment_id: nil)
