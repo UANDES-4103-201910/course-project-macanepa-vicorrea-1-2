617a6e918c80c6870cf97f1570049d705e9c287c
Matias Canepa <macanepa@miuandes.cl>
2019-05-10 19:54:45 -0400

    Only owner can edit/delete his post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -77,6 +77,9 @@ class PostsController < ApplicationController
     end
   end
 
+
+
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -6,6 +6,7 @@ class UserProfileController < ApplicationController
     end
     @user_profile = Profile.where(user_id: user)[0]
     @user_posts = Post.where(user_id: user)[0]
+    @posts_no_dumpster = Post.where("posts.id NOT IN (?)", Dumpster.pluck(:post_id))
 
     @vals = Validation.where(user_id: user)
     @vals += SharedPost.where(user_id: user)
