40fb18be52ade0bb6d4ae8dc7fa325bd5ac8721e
Vicentecorrea <vicorrea@miuandes.cl>
2019-06-02 18:26:56 -0400

    Added report post in admin section

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -28,7 +28,7 @@ class ReportsController < ApplicationController
 
     respond_to do |format|
       if @report.save
-        format.html { redirect_to @report, notice: 'Report was successfully created.' }
+        format.html { redirect_back(fallback_location: root_path); flash[:notice] = 'The post was successfully marked as inappropriate.' }
         format.json { render :show, status: :created, location: @report }
       else
         format.html { render :new }
