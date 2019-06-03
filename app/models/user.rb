a9b160b7120a47b6f161c91dff77faed2ad81110
Vicentecorrea <vicorrea@miuandes.cl>
2019-06-02 21:56:00 -0400

    Details

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
   has_many :tags, dependent: :delete_all
 
   # validates :email, presence: true, format: {with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/}, uniqueness: true
-  # validates :name, :last_name, :password, presence: true
+  validates :name, presence: true
 
   def self.from_omniauth(auth)
     where(provider: auth.provider ,uid: auth.uid).first_or_create do |user|
