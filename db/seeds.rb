df8f280449c87332ad46526c252267d48deb361e
Matias Canepa <macanepa@miuandes.cl>
2019-06-02 22:52:40 -0400

    Final Push Assignment 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ user06 = User.create!(name:"Mati",last_name:"Canepa",email:"matiasalbertocg@gmai
 
 #Profiles
 profile03 = Profile.create!(user_id:user03.id,biography:"Soy muy enojon. Me enojo por todo >:[",country:"Chile",city:"Santiago")
-profile04 = Profile.create!(user_id:user04.id,biography:"rotsap im se natas",country:"Italia",city:"Milano")
+profile04 = Profile.create!(user_id:user04.id,biography:"They call me Bad Choices. Got it memorized?",country:"Italia",city:"Milano")
 profile05 = Profile.create!(user_id:user05.id,biography:"rotsap im se natas",country:"China",city:"Milano")
 profile06 = Profile.create!(user_id:user06.id,biography:"La Matucha",country:"Chile",city:"Santiago")
 
@@ -21,7 +21,7 @@ admin03 = Admin.create!(user_id:user05.id,super_admin:true,geofence:nil)
 
 #Posts
 post01 = Post.create!(user_id:user03.id,title:"Titulo",content:"Post de prueba",is_open:true,is_solved:false)
-post02 = Post.create!(user_id:user04.id,title:"Titulo 2",content:"Post de prueba 2",is_open:false,is_solved:true)
+post02 = Post.create!(user_id:user04.id,title:"Hey listen",content:"I'm quite recognized for my eloquent decision making. People often say that I take 'Bad Choices'",is_open:false,is_solved:true)
 
 
 # Comments
