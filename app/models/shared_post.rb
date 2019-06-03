724f9a6ac83e15e1ffc3971ffa2d7efef8926d06
Matias Canepa <macanepa@miuandes.cl>
2019-06-02 21:21:00 -0400

    Hot fix en reply a reply

--- app/models/shared_post.rb
+++ app/models/shared_post.rb
@@ -5,4 +5,12 @@ class SharedPost < ApplicationRecord
   validates :user_id, :post_id, presence: true
   validates :user_id, uniqueness: { scope: [:post_id] }
 
+  # def check_user
+  #   puts("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+  #   r= user_id != Post.find(post_id).user_id
+  #   puts(r)
+  #   puts("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+  #   return r
+  # end
+
 end
