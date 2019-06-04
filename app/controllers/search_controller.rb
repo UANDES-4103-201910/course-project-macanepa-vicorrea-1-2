b8b137e13583c394d30b6d072babaf9943f5bc9a
Matias Canepa <macanepa@miuandes.cl>
2019-06-04 09:43:02 -0400

    Massive changes in search engine

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -20,7 +20,12 @@ class SearchController < ApplicationController
       end
     end
 
+    @users_name = User.where("name LIKE (?) or last_name LIKE (?)", "%#{params[:input]}%", "%#{params[:input]}%")
+    @users_location = User.joins(:profile).where("city LIKE (?) or country LIKE (?)","%#{params[:input]}%","%#{params[:input]}%")
+    @posts_content = Post.where("content LIKE (?) or title LIKE (?)","%#{params[:input]}%","%#{params[:input]}%")
+    @posts_author = Post.joins(:user).where("name LIKE (?) or last_name LIKE (?)","%#{params[:input]}%","%#{params[:input]}%")
 
     render("search/search")
   end
+
 end
