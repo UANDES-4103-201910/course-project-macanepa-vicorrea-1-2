51874cf4cb50964b63da8a85df061d1b3640e33b
Matias Canepa <macanepa@miuandes.cl>
2019-06-10 14:04:56 -0400

    Fixed Reload map

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,6 +27,12 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+    in_dumpster = Dumpster.where(post_id:@post.id).where(exit_date: nil).length>0
+    if in_dumpster
+      if not (user_signed_in? && (not current_user.admin.nil?))
+          redirect_to root_path, alert: "You don't have the privilages to see this post"
+      end
+    end
   end
 
   # GET /posts/new
--- app/controllers/watch_controller.rb
+++ app/controllers/watch_controller.rb
@@ -3,9 +3,16 @@ class WatchController < ApplicationController
   def watch
     if(params[:post_id] != nil)
       @posts = Post.find(params[:post_id])
+
+      in_dumpster = Dumpster.where(post_id:@posts.id).where(exit_date: nil).length>0
+      if in_dumpster
+        if not (user_signed_in? && (not current_user.admin.nil?))
+          redirect_to root_path, alert: "You don't have the privilages to see this post"
+        end
+      else
       render("watch/watch")
+      end
     end
   end
 
 end
-
